595b89ec151e175f28b5755a755b79d797568f90
Trokia003 <37078152+Trokia003@users.noreply.github.com>
2018-04-16 17:27:29 -0500

    unimportant

--- painting-app/src/ColorAndSizeContents.jsx
+++ painting-app/src/ColorAndSizeContents.jsx
@@ -15,10 +15,14 @@ class ColorAndSizeContents extends Component {
   render() {
     return (
       <div>
-        <label>Brush Color</label><br/>
-      <button className="color_and_size_button" onClick={this.showColorModal_ButtonClicked.bind(this)}></button><br/>
-        <label>Brush Size: </label><br/>
-        <input type="range" min="1" max="50" step="1"></input><br/>
+        <div>
+          <label>Brush Color</label><br/>
+          <button className="color_and_size_button" onClick={this.showColorModal_ButtonClicked.bind(this)}></button><br/>
+        </div>
+        <div>
+          <label>Brush Size: </label><br/>
+          <input type="range" min="1" max="50" step="1"></input><br/>
+        </div>
       </div>
     );
   }
